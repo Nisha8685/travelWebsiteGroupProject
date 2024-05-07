455c23465876c8dbc431302d6c26a9ebc15c4e69
samsunnishamultani <saihsaif8685@gmail.com>
2024-05-07 19:13:59 -0400

    new commit

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,6 +6,8 @@ from pages.login_page import LoginPage
 from pages.login_success_page import LoginSuccessPage
 from pages.Signoff_page import SignoffPage
 from pages.signoff_success_page import SignoffSuccessPage
+
+
 class TestLogin:
 
     # Test Case 1 - Registering the user
@@ -14,26 +16,26 @@ class TestLogin:
         index_page.navigate_to(TEST_SITE_URL)
         index_page.click_on_register_btn()
         register_page = RegisterPage(driver)
-        register_page.register_new_user(username_password[0],username_password[1])
+        register_page.register_new_user(username_password[0], username_password[1])
         register_success_page = RegisterSuccessPage(driver)
         register_success_lbl = register_success_page.get_register_success_label()
         assert "Note: Your user name is dan25." == register_success_lbl, "Test Case 1 Failed due to username assertion error"
 
     # Test Case 2
 
-    def test_login_user(self,driver,username_password):
+    def test_login_user(self, driver, username_password):
         login_page = LoginPage(driver)
         login_page.navigate_to(TEST_SITE_URL)
-        login_page.login_user(username_password[0],username_password[1])
-        login_success_page=LoginSuccessPage(driver)
-        login_success_lbl=login_success_page.get_login_success_label()
-        assert "Login Successfully" == login_success_lbl,"Test case 2 failed"
+        login_page.login_user(username_password[0], username_password[1])
+        login_success_page = LoginSuccessPage(driver)
+        login_success_lbl = login_success_page.get_login_success_label()
+        assert "Login Successfully" == login_success_lbl, "Test case 2 failed"
 
-    #Test Case 3
+    # Test Case 3
 
-    def test_signoff_user(self,driver):
+    def test_signoff_user(self, driver):
         signoff_page = SignoffPage(driver)
         signoff_page.signoff_user()
         signoff_success_page = SignoffSuccessPage(driver)
-        signoff_success_txt=signoff_success_page.get_signoff_success_btn()
-        assert "SIGN-ON" == signoff_success_txt,"Test case 2 failed"
+        signoff_success_txt = signoff_success_page.get_signoff_success_btn()
+        assert "SIGN-ON" == signoff_success_txt, "Test case 2 failed"
